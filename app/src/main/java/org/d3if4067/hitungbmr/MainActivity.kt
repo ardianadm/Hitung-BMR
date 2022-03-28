bb8980a1186cacb4405b65038048d0debcd98e74
Ardian Adam Alfarisyi <ardian.adam16@gmail.com>
2022-03-28 15:51:03 +0700

    Menambahkan view binding

--- app/src/main/java/org/d3if4067/hitungbmr/MainActivity.kt
+++ app/src/main/java/org/d3if4067/hitungbmr/MainActivity.kt
@@ -2,10 +2,14 @@ package org.d3if4067.hitungbmr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if4067.hitungbmr.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
